43dd2680ae88a2dd61e606861303af97633aaa8c
mohammad.soliman44@gmail.com <mohammad.soliman44@gmail.com>
2022-07-28 12:51:07 +0200

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express, { Request, Response, NextFunction } from 'express'
 import morgan from 'morgan'
 import router from './routes/index';
 import ErrorHandler from './middlewares/ErrorHandler'
+import cors from 'cors'
+
 
 const app: express.Application = express()
 const address: string = "127.0.0.1:"+ process.env.NODE_PORT
 
 app.use(express.json())
 app.use(express.urlencoded());
+app.use(cors())
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
